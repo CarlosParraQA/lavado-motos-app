2ff7e3d0e114d03f882657d9ef31f755cdd292c1
Carlos Parra <cparra@nutrabiotics.com.co>
2026-05-20 03:23:56 -0500

    cambio nombres tablas

--- navbar.py
+++ navbar.py
@@ -210,7 +210,7 @@ def navbar():
         st.session_state.vista = "Inicio"
 
     col1, col2, col3, col4, col_user, col_logout = st.columns(
-        [0.5, 0.5, 0.5, 0.5, 1.0, 1.0],
+        [1, 1.8, 1.4, 1.5, 0.8, 0.8],
         vertical_alignment="center"
     )
 
