23b5ecce58e3d6f7d21c8ab3341885f6797ebc60
Carlos Parra <cparra@nutrabiotics.com.co>
2026-05-20 03:03:52 -0500

    cambio en lavadas del dia

--- navbar.py
+++ navbar.py
@@ -217,7 +217,7 @@ def navbar():
         "Inicio": "Inicio",
         "Registrar": "Registrar Nuevo Servicio",
         "Lavadas": "Servicios del Día",
-        "Cierre": "Próximamente nuevo módulo",
+        #"Cierre": "Próximamente nuevo módulo",
         "Historial": "Historial General"
     }
 
