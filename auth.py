d8ada9a8c4b415b9530724afd4559f70cab268a9
Carlos Parra <cparra@nutrabiotics.com.co>
2026-09-24 13:05:49 -0500

    acultar

--- auth.py
+++ auth.py
@@ -38,7 +38,7 @@ st.markdown("""
 <style>
 
 /* Ocultar botón inferior de administración de Streamlit */
-[data-testid="appCreatorAvatar"] {
+a[href="https://share.streamlit.io/user/carlosparraqa"] {
     display: none !important;
 }
 
